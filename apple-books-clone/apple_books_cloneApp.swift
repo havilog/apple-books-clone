eb0147c35064b99a1facdfc09eb046c2b45b75a6
havi-log <havi.log@kakaocorp.com>
2023-01-18 14:49:57 +0900

    create project

---
+++ apple-books-clone/apple_books_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_books_cloneApp.swift
+//  apple-books-clone
+//
+//  Created by havi.log on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct apple_books_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
